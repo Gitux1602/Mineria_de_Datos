eec7a1c80e11ccc10f3323be83b0a05d3d67c4ab
Gitux1602 <gdelmercado@outlook.com>
2023-09-14 18:25:19 -0600

    Tarea 1

---
+++ Lee-.csv-con-Python.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd # Importamos la libreria pandas
+
+
+# In[4]:
+
+
+df_ligaArgentina = pd.read_csv('https://www.football-data.co.uk/new/ARG.csv') # Leemos el archivo csv
+
+
+# In[5]:
+
+
+df_ligaArgentina # Mostramos el DataFrame
+
+
+# In[ ]:
+
+
+
+
